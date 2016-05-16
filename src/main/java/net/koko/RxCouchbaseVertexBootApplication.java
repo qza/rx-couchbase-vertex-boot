0f04aeb1b451c6dcf1fafa8213932d71707484b7
qza <kokovic.zoran@gmail.com>
2016-05-16 07:40:34 +0200

    init project

---
+++ src/main/java/net/koko/RxCouchbaseVertexBootApplication.java
@@ -0,0 +1,12 @@
+package net.koko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxCouchbaseVertexBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxCouchbaseVertexBootApplication.class, args);
+	}
+}
